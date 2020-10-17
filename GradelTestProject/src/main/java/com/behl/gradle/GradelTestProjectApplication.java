fd32f30d0cd397313e7aa2b9e09b2749b4687ebe
Navneet <behlnavneet@gmail.com>
2020-10-17 10:49:52 +0100

    First Test commit on test project.

---
+++ GradelTestProject/src/main/java/com/behl/gradle/GradelTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.behl.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradelTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradelTestProjectApplication.class, args);
+	}
+
+}
