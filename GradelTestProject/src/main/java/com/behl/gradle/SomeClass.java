807e4f72e5136677db03c63c3e1c7e14b300780f
Navneet <behlnavneet@gmail.com>
2020-10-17 11:02:12 +0100

    The file had some errors...I have fixed it and reuploading it.

---
+++ GradelTestProject/src/main/java/com/behl/gradle/SomeClass.java
@@ -0,0 +1,9 @@
+package com.behl.gradle;
+
+public class SomeClass {
+
+	void method() {
+		System.out.println("This class updated by another Dev.");
+	}
+
+}
--- GradelTestProject/src/main/java/com/behl/gradle/TeamFileUpdate.java
+++
@@ -1,4 +0,0 @@
-public class SomeClass{
-
-This class updated by another Dev.
-}
